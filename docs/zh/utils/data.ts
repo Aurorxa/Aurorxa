4b534fc55596f36b109a6963d856391b18a5a51c
许大仙 <1900919313@qq.com>
2024-12-04 08:51:18 +0800

    add

--- docs/zh/utils/data.ts
+++ docs/zh/utils/data.ts
@@ -11,14 +11,14 @@ export const NAV_DATA: NavData[] = [
           text: '计算机科学',
           type: 'tip',
         },
-        desc: '计算机的发展史、二进制、指令和程序、数据结构与算法、人工智能、计算机视觉、自然语言处理等。',
+        desc: '计算机科学相关的基础知识，包括：计算机的发展史、二进制、指令和程序、数据结构与算法、人工智能、计算机视觉、自然语言处理等。',
         link: 'https://computer-science.weiweixu.cn/'
       },
       {
         icon: `/coa6/logo.svg`,
         title: '计组6件套',
         badge: {
-          text: '计算机基础',
+          text: '计组6件套',
           type: 'tip',
         },
         desc: 'C 语言、计算机组成原理、数据结构和算法、操作系统、计算机网络、设计模式。',
